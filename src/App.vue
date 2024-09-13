0d7c0a5cf86583fb4b989816fe782c398a7c8cf2
vitris128 <lenhattrong128@gmail.com>
2024-09-13 17:16:16 +0700

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+}
+</script>
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+
+h1 {
+  color: #42b983;
+  font-size: 2em;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
